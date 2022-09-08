c0ac0e937c530b15bd3da2bce1b9b966a58029d1
Intern-Yago <yagovictorbotafogo@gmail.com>
2022-09-07 21:25:07 -0300

    bestcleanbrasilia

--- js/script.js
+++ js/script.js
@@ -2,7 +2,10 @@ const wppLink = document.querySelector("#wpp_link")
 window.addEventListener("scroll", () =>{
     if(window.pageYOffset > 100){
         wppLink.classList.add('active')
-    }else{
+    }else if(window.pageYOffset == window.screen.height){
+        wppLink.classList.remove('active')
+    }
+    else{
         wppLink.classList.remove('active')
     }
 
